d7d28cba6ae7b7696066b01b2ddb5c1e4d3de3d0
Marat Bogatyrev <marat.bogatyrev@incountry.com>
2023-05-24 16:20:58 +0400

    111

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -2,7 +2,7 @@ from cassandra.cluster import Cluster
 import time
 
 # Connect to the Cassandra cluster
-cluster = Cluster(['127.0.0.1'], port=9042)
+cluster = Cluster(['172.28.250.2'], port=9042)
 session = cluster.connect()
 
 # Create a keyspace
@@ -10,7 +10,7 @@ session.execute("""
 CREATE KEYSPACE IF NOT EXISTS boost_3 
 WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};
 """)
-time.sleep(10)
+time.sleep(2)
 # Use the keyspace
 session.set_keyspace('boost_3')
 
@@ -21,7 +21,7 @@ Id int PRIMARY KEY,
 name text,
 city text);
 """)
-time.sleep(10)
+time.sleep(2)
 # Generate 200,000 rows of data
 for i in range(1, 1000):
     name = 'Name {}'.format(i)
