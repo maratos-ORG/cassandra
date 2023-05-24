d66ebc8e4385a06275ab37d78a749e7bfd5179b9
Marat Bogatyrev <marat.bogatyrev@incountry.com>
2023-05-24 22:53:43 +0400

    NEW_VER

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,27 +1,35 @@
 from cassandra.cluster import Cluster
+from cassandra import ConsistencyLevel
+from cassandra.query import SimpleStatement
 import time
 
 # Connect to the Cassandra cluster
 cluster = Cluster(['172.28.250.2'], port=9042)
 session = cluster.connect()
 
-# Create a keyspace
-session.execute("""
+# prepare your queries
+create_keyspace_query = """
 CREATE KEYSPACE IF NOT EXISTS boost_3 
-WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};
-""")
-time.sleep(2)
+WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3}
+"""
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS boost_3.mytable (
+  id int PRIMARY KEY,
+  name text
+)
+"""
+
+# create keyspace with consistency level of LOCAL_QUORUM
+statement = SimpleStatement(create_keyspace_query, consistency_level=ConsistencyLevel.LOCAL_QUORUM)
+session.execute(statement)
+
 # Use the keyspace
 session.set_keyspace('boost_3')
 
-# Create a table 
-session.execute("""
-CREATE TABLE IF NOT EXISTS tb1 (
-Id int PRIMARY KEY,
-name text,
-city text);
-""")
-time.sleep(2)
+# create table with consistency level of LOCAL_QUORUM
+statement = SimpleStatement(create_table_query, consistency_level=ConsistencyLevel.LOCAL_QUORUM)
+session.execute(statement)
 # Generate 200,000 rows of data
 for i in range(1, 1000):
     name = 'Name {}'.format(i)
